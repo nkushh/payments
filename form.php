f1e00cabfe0ec6cfdf743954efbdbb5f33685b3f
nkushh <mugunapiero@gmail.com>
2015-06-30 19:33:04 +0300

    Added a form to take users number and amount

---
+++ form.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Form</title>
+</head>
+<body>
+	<form action="" method="">
+		<label for="Phone number">Phone No.</label><br>
+		<input type="text" name="phone_no" placeholder="Phone No."><br>
+		<label for="amount">Amount</label><br>
+		<input type="text" name="amount" placeholder="Amount">
+	</form>
+</body>
+</html>
